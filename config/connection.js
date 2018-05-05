f5dac826d1d556beb143960e4eaa493d7d457f25
jkrowden <jkrowden@gmail.com>
2018-05-05 10:58:22 -0600

    updating files on lt

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,7 @@
 var mysql = require("mysql");
 
-module.exports = connection = mysql.createConnection({
-    hostname: 'localhost',
-    port: 3306,
-
-    user: 'root',
-    password: 'goober',
-    database: 'burgers_db'
+module.exports = connection =  mysql.createConnection({
+    hostname: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'b8519a36521607',
+    database: 'heroku_0b07429bb8e153'
 });
